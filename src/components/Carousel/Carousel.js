5ee058a63ad2f6710b4fc6be2ae25ce5e63f7378
Sumedhj16 <sjouras@gmail.com>
2020-07-19 15:57:46 +0530

    Add Files

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+import './Carousel.css';
+
+export default class Responsive extends Component {
+    render() {
+        var settings = {
+            dots: true,
+            infinite: false,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            initialSlide: 0,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        };
+        return (
+            <div className="Carousel">
+                <Slider {...settings}>
+                    <div className="card">
+                        <p>Savanna HR have always shown sincere interest in the deliverables for skills across levels. Their participation in requirement understanding commendable. I have always relied on for their round the clock availability.</p>
+                        <span>Tanvi Jain</span><br />
+                        <span>HR, Pearson Education</span><br />
+                    </div>
+                    <div className="card">
+                        <p>I have worked very closely with the Savanna HR team and they have helped us to close many niche hires. I find the team committed to quality service delivery and open to feedback.</p>
+                        <span>Anustup Nayak</span><br />
+                        <span>VP, XSEED Education</span>
+                    </div>
+                    <div className="card">
+                        <p>Savanna HR is an excellent Recruitment firm for EdTech Hiring. I was guided throughout and helped with an excellent opportunity in Education Industry. Team is very helpful with good network.</p>
+                        <span>Monica Singh</span><br />
+                        <span></span>
+                    </div>
+                    <div className="card">
+                        <p>Savanna HR have always shown sincere interest in the deliverables for skills across levels. Their participation in requirement understanding commendable. I have always relied on for their round the clock availability.</p>
+                        <span>Tanvi Jain</span><br />
+                        <span>HR, Pearson Education</span><br />
+                    </div>
+                    <div className="card">
+                        <p>I have worked very closely with the Savanna HR team and they have helped us to close many niche hires. I find the team committed to quality service delivery and open to feedback.</p>
+                        <span>Anustup Nayak</span><br />
+                        <span>VP, XSEED Education</span>
+                    </div>
+                    <div className="card">
+                        <p>Savanna HR is an excellent Recruitment firm for EdTech Hiring. I was guided throughout and helped with an excellent opportunity in Education Industry. Team is very helpful with good network.</p>
+                        <span>Monica Singh</span><br />
+                        <span></span>
+                    </div>
+                    <div className="card">
+                        <p>Savanna HR have always shown sincere interest in the deliverables for skills across levels. Their participation in requirement understanding commendable. I have always relied on for their round the clock availability.</p>
+                        <span>Tanvi Jain</span><br />
+                        <span>HR, Pearson Education</span><br />
+                    </div>
+                    <div className="card">
+                        <p>I have worked very closely with the Savanna HR team and they have helped us to close many niche hires. I find the team committed to quality service delivery and open to feedback.</p>
+                        <span>Anustup Nayak</span><br />
+                        <span>VP, XSEED Education</span>
+                    </div>
+                </Slider>
+            </div>
+        );
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+import './App.css';
+import Navbar from '../components/Navbar/Navbar.js';
+import Header from '../components/Header/Header.js';
+import Head from '../components/Head/Head.js';
+import SearchBar from '../components/SearchBar/SearchBar.js';
+import Team from '../components/Team/Team.js';
+import Title from '../components/Title/Title.js';
+import Talent from '../components/Talent/Talent.js';
+import Work from '../components/Work/Work.js';
+import Companies from '../components/Companies/Companies.js';
+import JobBoard from '../components/JobBoard/JobBoard.js';
+import Clients from '../components/Clients/Clients.js';
+import Blogs from '../components/Blogs/Blogs.js';
+import Hiring from '../components/Hiring/Hiring.js';
+import Footer from '../components/Footer/Footer';
+import Carousel from '../components/Carousel/Carousel'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Navbar />
+      <Head />
+      <SearchBar />
+      <Team />
+      <Title title="TALENTS" subTitle="The choice is yours, we’ve got you covered" />
+      <Talent />
+      <Title title="OUR SCOPE OF WORK" subTitle="Why Savannah?" />
+      <Work />
+      <Title title="OUR PRIDE" subTitle="Companies We Worked With" />
+      <Companies />
+      <Title title="WORK" subTitle="Introducing our job board - HireXL" />
+      <JobBoard />
+      <Title title="TESTIMONIALS" subTitle="What Our Clients Say" />
+      <Carousel />
+      <Title title="SOME WORD FOR BLOG" subTitle="Our Blogs" />
+      <Blogs />
+      <Hiring />
+      <Footer />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  );}
 
 export default IndexPage
